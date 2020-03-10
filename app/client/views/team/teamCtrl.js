9977436a42e67b3c2476015a7f1282209ab04360
Yasser El Habib DRIF <gy_drif@esi.dz>
2020-03-10 14:08:41 +0100

    added isTeamMember function

--- app/client/views/team/teamCtrl.js
+++ app/client/views/team/teamCtrl.js
@@ -23,6 +23,17 @@ angular.module('reg')
 
       $scope.user = currentUser.data;
 
+      function isTeamMember(teams,Userid) {
+        var test = false;
+        teams.forEach(team => {
+          team.members.forEach(member => {            
+            if (member.id==Userid) test = true;
+          });
+        });        
+        return test;
+      }
+
+      
       TeamService.getAll().then(teams => {
         $scope.isTeamAdmin=false;
         $scope.isTeamMember=false;
