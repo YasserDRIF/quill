df9bab14fd3863453c8a7a158deb0331f22b38bd
Abdelkhalek ZELLAT <ha_zellat@esi.dz>
2020-03-12 02:31:11 +0100

    add form validations

--- app/client/views/application/applicationCtrl.js
+++ app/client/views/application/applicationCtrl.js
@@ -9,7 +9,7 @@ angular.module('reg')
     'Session',
     'UserService',
     'MarketingService',
-    function($scope, $rootScope, $state, $http, currentUser, settings, Session, UserService, MarketingService) {
+    function ($scope, $rootScope, $state, $http, currentUser, settings, Session, UserService, MarketingService) {
 
       // Set up the user
       $scope.user = currentUser.data;
@@ -18,7 +18,7 @@ angular.module('reg')
       $scope.isHostSchool = $scope.user.email.split('@')[1] == settings.data.hostSchool;
 
       // If so, default them to adult: true
-      if ($scope.isHostSchool){
+      if ($scope.isHostSchool) {
         $scope.user.profile.adult = true;
       }
 
@@ -31,14 +31,14 @@ angular.module('reg')
 
       $scope.regIsClosed = Date.now() > settings.data.timeClose;
 
-      function populateSchools(){
+      function populateSchools() {
         $http
           .get('/assets/schools.json')
-          .then(function(res){
+          .then(function (res) {
             var schools = res.data;
             var email = $scope.user.email.split('@')[1];
 
-            if (schools[email]){
+            if (schools[email]) {
               $scope.user.profile.school = schools[email].school;
               $scope.autoFilledSchool = true;
             }
@@ -46,131 +46,131 @@ angular.module('reg')
 
         $http
           .get('/assets/schools.csv')
-          .then(function(res){
+          .then(function (res) {
             $scope.schools = res.data.split('\n');
             $scope.schools.push('Other');
 
             var content = [];
 
-            for(i = 0; i < $scope.schools.length; i++) {
+            for (i = 0; i < $scope.schools.length; i++) {
               $scope.schools[i] = $scope.schools[i].trim();
-              content.push({title: $scope.schools[i]})
+              content.push({ title: $scope.schools[i] })
             }
 
             $('#school.ui.search')
               .search({
                 source: content,
                 cache: true,
-                onSelect: function(result, response) {
+                onSelect: function (result, response) {
                   $scope.user.profile.school = result.title.trim();
                 }
               })
           });
       }
-    
 
-      function populateWilayas(){
+
+      function populateWilayas() {
         $http
           .get('/assets/wilayas.csv')
-          .then(function(res){
+          .then(function (res) {
             $scope.wilayas = res.data.split('\n');
             $scope.wilayas.push('Other');
 
             var content = [];
 
-            for(i = 0; i < $scope.wilayas.length; i++) {
+            for (i = 0; i < $scope.wilayas.length; i++) {
               $scope.wilayas[i] = $scope.wilayas[i].trim();
-              content.push({title: $scope.wilayas[i]})
+              content.push({ title: $scope.wilayas[i] })
             }
 
             $('#wilaya.ui.search')
               .search({
                 source: content,
                 cache: true,
-                onSelect: function(result, response) {
+                onSelect: function (result, response) {
                   $scope.user.profile.wilaya = result.title.trim();
                 }
               })
           });
       }
-      
 
-      function populateClubs(){
+
+      function populateClubs() {
         $http
           .get('/assets/clubs.csv')
-          .then(function(res){
+          .then(function (res) {
             $scope.clubs = res.data.split('\n');
             $scope.clubs.push('Other');
 
             var content = [];
 
-            for(i = 0; i < $scope.clubs.length; i++) {
+            for (i = 0; i < $scope.clubs.length; i++) {
               $scope.clubs[i] = $scope.clubs[i].trim();
-              content.push({title: $scope.clubs[i]})
+              content.push({ title: $scope.clubs[i] })
             }
 
             $('#club.ui.search')
               .search({
                 source: content,
                 cache: true,
-                onSelect: function(result, response) {
+                onSelect: function (result, response) {
                   $scope.club = result.title.trim();
                 }
               })
           });
-          if ($scope.user.profile.source != undefined){
-            $scope.UserSource = $scope.user.profile.source.split('#')[0];
-            $scope.club = $scope.user.profile.source.split('#')[1];  
-          }
+        if ($scope.user.profile.source != undefined) {
+          $scope.UserSource = $scope.user.profile.source.split('#')[0];
+          $scope.club = $scope.user.profile.source.split('#')[1];
         }
+      }
 
 
       function removeDuplicates(myArr, prop) {
         return myArr.filter((obj, pos, arr) => {
-            return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
+          return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
         });
       }
 
-      function sendMarketingEmails(){
-        MarketingService.getAll().then(teams=>{
-          var emails=[];
+      function sendMarketingEmails() {
+        MarketingService.getAll().then(teams => {
+          var emails = [];
           teams.data.forEach(team => {
-            var isTeammate=false;
+            var isTeammate = false;
             team.members.forEach(member => {
-              if (member==currentUser.data.email){
-                isTeammate=true;
+              if (member == currentUser.data.email) {
+                isTeammate = true;
               }
             });
             if (isTeammate) {
               team.members.forEach(member => {
-                if (!(member==currentUser.data.email)){
-                  emails.push({email:member,event:team.event})
-                } 
+                if (!(member == currentUser.data.email)) {
+                  emails.push({ email: member, event: team.event })
+                }
               });
             }
           });
-          removeDuplicates(emails,'email').forEach(teammate => {
-            MarketingService.sendFriendInvite(currentUser.data.profile.name,teammate)
+          removeDuplicates(emails, 'email').forEach(teammate => {
+            MarketingService.sendFriendInvite(currentUser.data.profile.name, teammate)
           });
         })
       }
 
 
-      function _updateUser(e){
+      function _updateUser(e) {
 
         //Check if User's first submission
         var sendMail = true;
-        if (currentUser.data.status.completedProfile) {sendMail=false}        
+        if (currentUser.data.status.completedProfile) { sendMail = false }
 
         // Get user Source
-        if ($scope.UserSource!='2'){$scope.user.profile.source=$scope.UserSource}
-          else{$scope.user.profile.source=$scope.UserSource+"#"+$scope.club}
+        if ($scope.UserSource != '2') { $scope.user.profile.source = $scope.UserSource }
+        else { $scope.user.profile.source = $scope.UserSource + "#" + $scope.club }
 
         UserService
           .updateProfile(Session.getUserId(), $scope.user.profile)
           .then(response => {
             swal("Awesome!", "Your application has been saved.", "success").then(value => {
-              if (sendMail){ sendMarketingEmails(); }
+              if (sendMail) { sendMarketingEmails(); }
               $state.go("app.dashboard");
             });
           }, response => {
@@ -195,14 +195,15 @@ angular.module('reg')
         return true;
       }
 
-      function _setupForm(){
+      function _setupForm() {
         // Custom minors validation rule
         $.fn.form.settings.rules.allowMinors = function (value) {
           return minorsValidation();
         };
 
-        // Semantic-UI form validation
+        // Semantic-UI form valid ation
         $('.ui.form').form({
+          on: 'blur',
           inline: true,
           fields: {
             name: {
@@ -223,8 +224,8 @@ angular.module('reg')
                 }
               ]
             },
-            wilaya: {
-              identifier: 'wilaya',
+            Wilaya: {
+              identifier: 'Wilaya',
               rules: [
                 {
                   type: 'empty',
@@ -267,13 +268,77 @@ angular.module('reg')
                   prompt: 'You must be an adult, or an ESI student.'
                 }
               ]
-            }
+            },
+            study: {
+              identifier: 'study',
+              rules: [
+                {
+                  type: 'empty',
+                  prompt: 'Please enter your major .'
+                }
+              ]
+            },
+            description: {
+              identifier: 'description',
+              rules: [
+                {
+                  type: 'empty',
+                  prompt: 'Please describe yourself .'
+                }
+              ]
+            },
+            essay: {
+              identifier: 'essay',
+              rules: [
+                {
+                  type: 'empty',
+                  prompt: 'Please tell us about your motivation .'
+                }
+              ]
+            },
+            linkedin: {
+              identifier: 'linkedin',
+              rules: [
+                {
+                  type: 'url',
+                  prompt: 'Please enter a valid Linkedin URL .'
+                }
+              ]
+            },
+            facebook: {
+              identifier: 'facebook',
+              rules: [
+                {
+                  type: 'url',
+                  prompt: 'Please enter a valid Facebook URL .'
+                }
+              ]
+            },
+            github: {
+              identifier: 'github',
+              rules: [
+                {
+                  type: 'url',
+                  prompt: 'Please enter a valid GitHub URL .'
+                }
+              ]
+            },
+            UserSource: {
+              identifier: 'UserSource',
+              rules: [
+                {
+                  type: 'empty',
+                  prompt: 'How did you hear about us ?'
+                }
+              ]
+            },
           }
         });
       }
 
-      $scope.submitForm = function(){
-        if ($('.ui.form').form('is valid')){
+      $scope.submitForm = function () {
+        if ($('.ui.form').form('is valid')) {
+          // $('.ui.submit.button').click();
           _updateUser();
         } else {
           swal("Uh oh!", "Please Fill The Required Fields", "error");
